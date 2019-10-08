2a7caacff7de038c07e918215592bb1d404842e8
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2019-10-08 15:27:26 -0300

    Alter 'include/artigos.php' for comments

--- include/artigos.php
+++ include/artigos.php
@@ -1,4 +1,8 @@
 <?php 
+	
+	// Visualização da thumbnail do "artigo da semana".
+	// English version: View on the thumbnail for "artigo da semana".
+	
 	echo "<button  class='button'><img class='aparecer' src='visualizarImagem.php?CODARTIGOS=$row[CODARTIGOS]'></button>
 			<figcaption class='um'><b>$row[TITULO]</b></figcaption>
 			<figcaption class='dois'>
